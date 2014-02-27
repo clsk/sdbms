16764f3accb6ea2e112641372f4e99699049e91c
isisgomez28 <igomezp.28@gmail.com>
2014-02-27 02:07:01 -0400

    Schema Fix

--- fs/Pair.java
+++ fs/Pair.java
@@ -1,6 +1,13 @@
-public class Pair<T, V> {
-	private T key;
-	private V value;
+import java.lang.Number;
+
+public class Pair <T, V> {
+	private T key = null;
+	private V value = null;
+	
+	public Pair (T theKey, V theValue){
+		key = theKey;
+		value = theValue;
+	}
 	
 	public T getKey(){
 		return this.key;
--- fs/Schema.java
+++ fs/Schema.java
@@ -1,6 +1,7 @@
 package fs;
 
 import java.util.HashMap;
+import java.lang.Integer;
 
 public class Schema
 {
@@ -10,16 +11,40 @@ public class Schema
         this.name = name;
     }
 
-    public void addField(String name, Pair <Integer, Integer> PosLength)
+    public void addField(String name, Pair <Integer, Integer> size)
     {
-    	PosLength.pair = PosLength.pair * 2;
-    	fields.put(name, PosLength);
+    	Integer var = size.getValue().intValue() * 2;
+    	Integer pos = size.getKey().intValue();
+    	Pair <Integer, Integer> aux = new Pair <Integer, Integer> (pos, var);
+    	fields.put(name, aux);
+
+    	var = null;
+    	pos = null;
+    	aux = null;
     }
 
-    public String getName() {
+    public void addField (String _name, Integer _pos, Integer _size) {
+    	Integer var = _size.intValue() * 2;
+    	Integer pos = _pos.intValue();
+    	Pair <Integer, Integer> aux = new Pair <Integer, Integer> (pos, var);    	
+    	fields.put(name, aux);
+
+    	var = null;
+    	pos = null;
+    	aux = null;
+    }
+    
+    /*
+     *	Funcion para evaluar la existencia del Field. 
+     */
+    private boolean checkField (){
+    	return false;
+    }
+    
+    public String getSchemaName() {
 		return name;
 	}
 
-	private HashMap<String, Pair<Integer, Integer>> fields;
+	private HashMap <String, Pair <Integer, Integer>> fields;
     private String name = null;
 }
