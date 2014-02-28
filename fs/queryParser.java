87e94ff88b5c7735d390120e9b0adc00c6dd027f
isisgomez28 <igomezp.28@gmail.com>
2014-02-28 12:25:24 -0400

    QueryParser para CreateTable
    Testeado, vean el comentario.

--- fs/queryParser.java
+++ fs/queryParser.java
@@ -29,30 +29,51 @@ public class queryParser {
 	}
 	
 	private void splitQuery() {
-		Pattern myPattern = Pattern.compile("(?m)^(create table|CREATE TABLE)\\s[\\w]+\\s\\(");
-		Matcher matching = myPattern.matcher("cadena");
+		/*
+		 * Create Table Super Query
+		 * Responde a :
+		 * Create Table NameTable (
+		 * atributoName varchar length,
+		 * atributoName varchar length,
+		 * );
+		 * 
+		 * Testeado :P
+		 */
+		Pattern myPattern = Pattern.compile("(?m)^(create table|CREATE TABLE)\\s[\\w]+\\s\\(");		
 		
-		if (matching.find()) {
-			for (int i = 0; i < lines.size(); i++) {
-				myPattern = Pattern.compile("(?m)[\\w]+\\s(varchar|VARCHAR)\\s[\\d]+(,[\\n[\\w]+\\s(varchar|VARCHAR)\\s[\\d]+]+)\\)?;$");
-				matching = myPattern.matcher(lines.elementAt(i + 1));
-				String [] aux = lines.elementAt(i).split("\\s");
-				if (matching.find()) {
-					table = (aux[2]);
-					aux = null;
-					aux = lines.elementAt(i).split("\\s");
-					atributes = new Vector <Pair <String, Integer>> ();
-					Pair <String, Integer> auxp = new Pair <String, Integer> ();
-					for (int J = 0; J < aux.length; J++){
-						if (aux[J] == "varchar" || aux [J] == "VARCHAR") {
-							//atriName, atriLength
-							auxp.setKey(aux[J - 1]);
-							auxp.setValue(Integer.parseInt(aux[J + 1]));
+		if (!lines.isEmpty()){
+			Matcher matching = myPattern.matcher(lines.elementAt(0));
+			if (matching.find()){
+				String line = lines.elementAt(0);
+				String [] aux = line.split("\\s");
+				setTable((aux[2]));
+				aux = null;				
+				for (int i = 1; i < lines.size(); i++){
+					myPattern = Pattern.compile("(?m)[\\w]+\\s(varchar|VARCHAR)\\s[\\d]+,$");
+					line = lines.elementAt(i);
+					matching = myPattern.matcher(line);
+					if (matching.find()){
+						if (atributes.isEmpty())
+							atributes = new Vector <Pair <String, Integer>> ();
+						Pair <String, Integer> auxp = new Pair <String, Integer> ();
+						line = lines.elementAt(i);
+						aux = line.split("\\s|\\,");
+						myPattern = Pattern.compile("(?m)^(varchar|VARCHAR)$");
+						line = aux[1];
+						matching = myPattern.matcher(line);
+						if (matching.find()) {
+							//atriName VARCHAR 10
+							auxp.setKey(aux[1 - 1]);
+							auxp.setValue(Integer.parseInt(aux[1 + 1]));
 							atributes.add(auxp);
 						}
 					}
-				}	
-			}			
+					myPattern = Pattern.compile("(?m)\\);$");
+					matching = myPattern.matcher(lines.elementAt(i));
+					if (matching.find())
+						break;
+				}
+			}
 		}
 		myPattern = Pattern.compile("(?m)^(INSERT)\\s[\\w]+(,\\s[\\w]+)?\\s(INTO)\\s[\\w]+\\s(VALUES)\\s[\\w]+(,\\s[\\w]+)?;$");
 			
